34c63ba192e23e4ee0effa9b421f2d4ffbfe43c9
Kristján Patrekur <krillinn@gmail.com>
2015-12-06 18:49:20 +0100

    somthing

--- Code/java/main.js
+++ Code/java/main.js
@@ -2,13 +2,13 @@
 $(document).ready(function() {
 	$('#datepicker').datepicker();
 	$('#datepicker').on("changeDate", function() {
-    $('#my_hidden_input').val(
-        $('#datepicker').datepicker('getFormattedDate')
-    );
-});
+		$('#my_hidden_input').val(
+			$('#datepicker').datepicker('getFormattedDate')
+		);
+	});
 });
 
 /* Contact info */
 function Contact(){
-    document.getElementById("info").style.visibility="visible";
+	document.getElementById("info").style.visibility="visible";
 }
