2954375232e0783facaff8ca2766a743025cddbb
Kristján Patrekur <krillinn@gmail.com>
2015-12-08 22:45:39 +0100

    indention fixes

--- Code/java/main.js
+++ Code/java/main.js
@@ -1,11 +1,13 @@
 
 $(document).ready(function() {
+	// Initialize calender (Jquery plug-in)
 	$('#datepicker').datepicker();
 	$('#datepicker').on("changeDate", function() {
 		$('#my_hidden_input').val(
 			$('#datepicker').datepicker('getFormattedDate')
 		);
 	});
+	// When pushing the search button you are redirected to your result
 	$("#searchBotton").click(function() {
 		location.href = "./Search.html";
 	});
